06b4d1a89b63b55bf3ba8327cac64b637a3ba2d0
Rino Vargas <rinovq@gmail.com>
2024-01-23 02:04:08 -0500

    Add logger name

--- logger/__init__.py
+++ logger/__init__.py
@@ -2,8 +2,8 @@ import logging
 import sys
 
 
-def get_new_logger():
-    logger = logging.getLogger()
+def get_new_logger(name=None):
+    logger = logging.getLogger() if name is None else logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler(sys.stdout))
     return logger
