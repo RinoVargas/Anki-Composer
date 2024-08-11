529835902d7d37a9c088cd5f209cd653dce18942
Rino Vargas <rino.vargas.quintana@gmail.com>
2024-08-10 21:21:02 -0500

    Fix tests

--- test/compose/deck_composer_test.py
+++ test/compose/deck_composer_test.py
@@ -29,7 +29,7 @@ class DeckComposerTest(unittest.TestCase):
         result = getattr(composer, '_specifications')
         back_template: BackTemplate | None = getattr(result[0], 'back_template')
         self.assertIsNotNone(back_template)
-        self.assertEquals(back_template.value,
+        self.assertEqual(back_template.value,
                           '<i>Meaning:</i> {{MEANING}}\n<hr id=answer>\n<i>Examples:</i> \n<ul>\n        <li>{{EXAMPLE_1}} {{$_EXAMPLE_1}}</li>\n        <li>{{EXAMPLE_2}} {{$_EXAMPLE_2}}</li>\n        <li>{{EXAMPLE_3}} {{$_EXAMPLE_3}}</li>\n</ul>\n')
 
     def test_composer_specification_back_template_file(self):
@@ -38,20 +38,20 @@ class DeckComposerTest(unittest.TestCase):
         back_template: BackTemplate | None = getattr(result[0], 'back_template')
         back_template.html_file = BACK_TEMPLATE_FILE_PATH
         self.assertIsNotNone(back_template)
-        self.assertEquals(deck_builder.html_file_to_string(back_template.html_file), '<div>\n    <p>This is the back_template: {{MEANING}}</p>\n</div>')
+        self.assertEqual(deck_builder.html_file_to_string(back_template.html_file), '<div>\n    <p>This is the back_template: {{MEANING}}</p>\n</div>')
 
     def test_composer_specification_deck_name_value(self):
         composer = DeckComposerTest.__create_composer()
         result = getattr(composer, '_specifications')
         deck_name = getattr(result[0], 'deck_name')
-        self.assertEquals(deck_name, 'English Golden List')
+        self.assertEqual(deck_name, 'English Golden List')
 
     def test_composer_specification_front_template_value(self):
         composer = DeckComposerTest.__create_composer()
         result = getattr(composer, '_specifications')
         front_template: FrontTemplate | None = getattr(result[0], 'front_template')
         self.assertIsNotNone(front_template)
-        self.assertEquals(front_template.value,
+        self.assertEqual(front_template.value,
                           "<div style='font-family: arial; font-size: 25px; text-align: center; color: white;'>{{EXPRESSION}}</div>")
 
     def test_composer_specification_front_template_file(self):
@@ -60,13 +60,13 @@ class DeckComposerTest(unittest.TestCase):
         front_template: FrontTemplate | None = getattr(result[0], 'front_template')
         front_template.html_file = FRONTEND_TEMPLATE_FILE_PATH
         self.assertIsNotNone(front_template)
-        self.assertEquals(deck_builder.html_file_to_string(front_template.html_file), '<div>\n    <p>This is the frontend_template: {{MEANING}}</p>\n</div>')
+        self.assertEqual(deck_builder.html_file_to_string(front_template.html_file), '<div>\n    <p>This is the frontend_template: {{MEANING}}</p>\n</div>')
 
     def test_composer_specification_media_folder_path_value(self):
         composer = DeckComposerTest.__create_composer()
         result = getattr(composer, '_specifications')
         media_folder_path = getattr(result[0], 'media_folder_path')
-        self.assertEquals(media_folder_path, '/my/audio/folder')
+        self.assertEqual(media_folder_path, '/my/audio/folder')
 
     def test_composer_specification_text_field_value(self):
         composer = DeckComposerTest.__create_composer()
@@ -74,8 +74,8 @@ class DeckComposerTest(unittest.TestCase):
         fields = getattr(result[0], 'fields')
         field: DeckSpecificationField = fields[0]
 
-        self.assertEquals(field.generate_audio_file, False)
-        self.assertEquals(field.name, 'EXPRESSION')
+        self.assertEqual(field.generate_audio_file, False)
+        self.assertEqual(field.name, 'EXPRESSION')
 
     def test_composer_specification_audio_field_value(self):
         composer = DeckComposerTest.__create_composer()
@@ -104,16 +104,16 @@ class DeckComposerTest(unittest.TestCase):
         result = getattr(composer, '_specifications')
         input_config: DeckInputConfig = getattr(result[0], 'input_config')
 
-        self.assertEquals(input_config.type, 'xlsx')
-        self.assertEquals(input_config.file_path, '/my/specific/file.xlsx')
+        self.assertEqual(input_config.type, 'xlsx')
+        self.assertEqual(input_config.file_path, '/my/specific/file.xlsx')
 
     def test_composer_specification_output_config_value(self):
         composer = DeckComposerTest.__create_composer()
         result = getattr(composer, '_specifications')
         output_config: DeckOutputConfig = getattr(result[0], 'output_config')
 
-        self.assertEquals(output_config.filename, 'my-package.dpkg')
-        self.assertEquals(output_config.folder_path, '/my/output/folder')
+        self.assertEqual(output_config.filename, 'my-package.dpkg')
+        self.assertEqual(output_config.folder_path, '/my/output/folder')
 
 
 if __name__ == '__main__':
