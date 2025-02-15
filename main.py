12f61e6379033c15515f06db047d896208d11eb6
Rino Vargas <rino.vargas.quintana@gmail.com>
2025-02-14 23:04:31 -0500

    Adding cli

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import sys
+from argparse import ArgumentParser
 
 from compose.deck_composer import DeckComposer
 
+DESCRIPTOR_PATH_ARG = "descriptor-path"
+
+
+def create_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument(DESCRIPTOR_PATH_ARG)
+    return parser
+
 
 def main():
-    deck_composer_file = sys.argv[1]
+    parser = create_parser()
+    result = vars(parser.parse_args())
+    deck_composer_file = result.get(DESCRIPTOR_PATH_ARG)
     compose = DeckComposer(deck_composer_file)
     compose.compose()
 
